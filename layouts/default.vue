f49b584b77cd2f701cdf11b94570e09ff330ade8
thistle519 <shizuru519@gmail.com>
2021-01-13 16:40:03 +0900

    top-barをdefaultに移した

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <TopBar/>
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <v-app>
-      <TopBar/>
       <v-main>
         <Header/>
         <SubHeader/>
