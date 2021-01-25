9da13c3b93d5be183f3c5cdb6c4e7da45ee437a8
thistle519 <shizuru519@gmail.com>
2021-01-25 12:36:29 +0900

    cardをindex.vueに読み込んだ

--- components/card.vue
+++ components/card.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         <!--このdivの間にかいていってね-->
-<div class="card is-4">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://versions.bulma.io/0.7.0/images/placeholders/1280x960.png" alt="Placeholder image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-content">
-        <p class="title is-4">カード</p>
-      </div>
-    </div>
+      <div class="card is-4">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="https://versions.bulma.io/0.7.0/images/placeholders/1280x960.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">カード</p>
+            </div>
+          </div>
 
-    <div class="content">
-     内容内容内容内容内容内容内容が無いよう内容内容内容兄ようあのユアナオ内容内容兄用兄アナy言おうあのユアにょあう矢野やうみゃおうやもy
-    </div>
-  </div>
-</div>
+          <div class="content">
+          内容内容内容内容内容内容内容が無いよう内容内容内容兄ようあのユアナオ内容内容兄用兄アナy言おうあのユアにょあう矢野やうみゃおうやもy
+          </div>
+        </div>
+      </div>
 
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,18 @@
         <Header/>
         <SubHeader/>
         <Main/>
+        <Card/>
       </v-main>
     </v-app>
   </div>
 </template>
 
 <script>
-export default {}
+import Card from '@/components/Card.vue'
+export default {
+  components: { Card },
+
+}
 </script>
 
 <style>
