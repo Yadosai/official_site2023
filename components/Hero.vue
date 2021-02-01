46422afdd36be8bb10d18b9081e61027496ae5ea
Mattun1212 <kouta.dvlp@icloud.com>
2021-02-02 03:00:52 +0900

    Hero画像部分を作った

---
+++ components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                    やどかり祭
+                </h1>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.hero {
+    background-image: url(https://bulma.io/images/placeholders/640x480.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+}
+.title {
+    color: $BLUE;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template>
   <section>
     <Header/>
+    <Hero/>
     <Adcard/>
   </section>
 </template>
 
 <script>
+
 import Adcard from '@/components/Adcard';
+import Hero from '@/components/Hero';
 export default {
   components:{
       Adcard,
+      Hero
     }
 }
 </script>
