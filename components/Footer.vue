dc07b0c0d816f4e0a98d358d181f26745bc62766
thistle519 <shizuru519@gmail.com>
2021-02-01 15:47:44 +0900

    footerをテーブルで作った

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,9 +7,20 @@
                         <p></p>
                     </div>
                     <div class="column">
-                        <p>お祭り情報</p>
-                        <div class="line"></div>
-                        <p>こんな感じ&nbsp;アクセス&nbsp;こんな感じ&nbsp;どうかな</p>
+                        <!--<div class="contents">
+                            <p>お祭り情報</p>
+                            <div style="background-color:gray; width:2px; height:40px;"></div>
+                            <p>こんな感じ&nbsp;アクセス&nbsp;こんな感じ&nbsp;どうかな</p>
+                        </div> -->
+                        <ul>
+                            <li>
+                                <h2>トップ</h2>
+                            </li>
+                            <li>
+                                <h2>チャットサポート</h2>
+                                <p>チャットサポートでは毎日のサポート時間内に随時質問ができ、即回答をもらうことができます。</p>
+                            </li>
+                        </ul>                        
                     </div>
                     <div class="column">
                         <p>どうもどうも</p>
@@ -31,9 +42,30 @@ export default {
     background-size: cover;
     color: #ffffff
 }
-.line{
-    margin: 0 1rem;
-    width: 4px;
-    background-color: #000;
+.contents{
+    display: flex;
+    p{
+        margin: 0.5em;
+    }
+}
+
+ul{
+  list-style:none;
+  width:100%;
+  display:flex;
+  justify-content:space-between;
+  li{
+    width:48%;
+    padding:0 10px;
+    &:first-child{ 
+      border-right:1px solid #ccc;
+    }
+    h2{
+      font-size:16px;
+    }
+    p{
+      font-size:12px
+    }
+  }
 }
 </style>
