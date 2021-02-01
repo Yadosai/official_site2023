dff1856ffc7bcff8ebd50f7b791400800f6353dd
Mattun1212 <kouta.dvlp@icloud.com>
2021-02-02 02:29:06 +0900

    広告カードを作りこみ

---
+++ components/Adcard.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div>
+        <div class="box row">
+         <a href="https://yadosai.com" target="_blank">
+             <img src="https://bulma.io/images/placeholders/640x480.png" alt="sponsor" class="web-ad-content">
+         </a>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+.row{
+  display: flex;
+  flex-wrap: wrap;
+}
+.box {
+  padding: 0.5em;
+  margin: 0.5em 1em;
+  border-radius: 10px;
+}
+.box:hover {
+  box-shadow: 0px 0px 0px 0px black;
+}
+.web-ad-content {
+  height: 148px;
+  width: 228px;
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <section>
     <Header/>
+    <Adcard/>
   </section>
 </template>
 
 <script>
+import Adcard from '@/components/Adcard';
+import SubHeader from '@/components/SubHeader';
 export default {
+  components:{
+      Adcard,
+      SubHeader
+    }
 }
 </script>
 
