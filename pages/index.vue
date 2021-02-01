41932dbafeb0f023db91ce105757769800a6e1ca
tamachan <tamachandazoi@yahoo.ne.jp>
2021-02-01 16:49:30 +0900

    1つ帯をつくったよ

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <section>
     <Header/>
+    <SubHeader/>
   </section>
 </template>
 
 <script>
+import SubHeader from '@/components/SubHeader'
 export default {
+  components: {
+    SubHeader
+  }
 }
 </script>
 
