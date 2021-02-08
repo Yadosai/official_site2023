a754aae37ce9aa666ae58ae2e95922cba36b3109
thistle519 <shizuru519@gmail.com>
2021-02-08 15:20:10 +0900

    真ん中を書いた

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,24 +7,29 @@
                         <p></p>
                     </div>
                     <div class="column">
-                        <!--<div class="contents">
-                            <p>お祭り情報</p>
-                            <div style="background-color:gray; width:2px; height:40px;"></div>
-                            <p>こんな感じ&nbsp;アクセス&nbsp;こんな感じ&nbsp;どうかな</p>
-                        </div> -->
-                        <ul>
-                            <li>
-                                <h2>トップ</h2>
-                            </li>
-                            <li>
-                                <h2>チャットサポート</h2>
-                                <p>チャットサポートでは毎日のサポート時間内に随時質問ができ、即回答をもらうことができます。</p>
-                            </li>
-                        </ul>                        
+                        <div class="contents">
+                            <p class="item">トップ</p>
+                            <div class="line">
+                                <p>こんな感じ&nbsp;アクセス&nbsp;こんな感じ&nbsp;どうかな&nbsp;どうかな&nbsp;どうかな</p>
+                            </div>
+                        </div>
+                        <div class="blank"></div>
+                        <div class="contents">
+                            <p class="item">お祭り情報</p>
+                            <div class="line">
+                                <p>アクセス&nbsp;マップ&nbsp;タイムテーブル&nbsp;模擬店&nbsp;企画&nbsp;浴衣コンテスト</p>
+                            </div>
+                        </div>                
                     </div>
                     <div class="column">
-                        <p>どうもどうも</p>
                     </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="content has-text-centered">
+                <p>©第47回宿舎祭実行委員会<br>
+                当サイトでは、アクセスログの収集・解析にGoogle アナリティクスを使用しております。
+Google アナリティクスではCookieを使用し、個人を特定する情報を含まずにログを収集しております。また、収集されるログはGoogleのプライバシーポリシーに基づいて管理されます。</p>
                 </div>
             </div>
         </div>
@@ -45,27 +50,26 @@ export default {
 .contents{
     display: flex;
     p{
-        margin: 0.5em;
+        margin: 1em 1em;
+        font-size: 0.7em;
     }
+}
+.blank{
+    height: 1em;
 }
 
-ul{
-  list-style:none;
-  width:100%;
-  display:flex;
-  justify-content:space-between;
-  li{
-    width:48%;
-    padding:0 10px;
-    &:first-child{ 
-      border-right:1px solid #ccc;
-    }
-    h2{
-      font-size:16px;
-    }
-    p{
-      font-size:12px
-    }
-  }
+.item{
+    width: 6em;
 }
+.line{
+    border-left: 0.1em solid $WHITE;
+    padding-left: 0.1em;
+}
+.content{
+    width: 30rem;
+    font-size:0.5em;
+    margin:0 auto;
+    padding-top: 3em;
+}
+
 </style>
