bf5eac3cc80d2691783b8ea548d37c3d0772cb70
Yataro Fujinaga <yatarofujinaga@gmail.com>
2021-03-03 18:00:41 +0900

    eslintとprettierの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,7 @@
-import * as FontAwesome from './builds/font-awesome';
-import * as EventsRouting from './builds/events-routing';
+import * as FontAwesome from './builds/font-awesome'
+import * as EventsRouting from './builds/events-routing'
 
 export default {
-
   target: 'static',
 
   // SSR is off
@@ -16,42 +15,33 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@/assets/scss/style.scss',
-  ],
+  css: ['@/assets/scss/style.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    { src: '@/plugins/vue-awesome-swiper', ssr: false }
-  ],
+  plugins: [{ src: '@/plugins/vue-awesome-swiper', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/bulma',
-    'nuxt-svg-loader'
-  ],
+  modules: ['@nuxtjs/bulma', 'nuxt-svg-loader'],
 
   buildModules: [
+    '@nuxtjs/eslint-module',
     '@nuxtjs/style-resources',
-    ['@nuxtjs/fontawesome', { component: 'fa', suffix: true }],
+    ['@nuxtjs/fontawesome', { component: 'fa', suffix: true }]
   ],
 
   generate: {
     routes() {
-      return EventsRouting.events.map(event => {
+      return EventsRouting.events.map((event) => {
         return `events/${event}`
       })
     }
@@ -66,15 +56,11 @@ export default {
   },
 
   styleResources: {
-    scss: [
-      '@/assets/scss/_mixins.scss',
-      '@/assets/scss/_variables.scss'
-    ]
+    scss: ['@/assets/scss/_mixins.scss', '@/assets/scss/_variables.scss']
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  },
+  build: {},
 
   svgLoader: {
     svgoConfig: {
