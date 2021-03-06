6d7bf4e08886263b88b20d6ce9066f7858aab653
Mattun1212 <kouta.dvlp@icloud.com>
2021-03-06 16:32:09 +0900

    Jumbotron作った

---
+++ components/Jumbotron.vue
@@ -0,0 +1,47 @@
+<template>
+   <section class="Jumbotron">
+    <h1 class="theme">お知らせ</h1>
+        <hr id="line">
+        <p class="content">
+        ・第47回やどかり祭は中止となりました。
+        </p>
+        <hr id="line">
+        <p class="content">
+        ・新歓実施中!
+        </p>
+        <hr id="line">
+</section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+
+.Jumbotron {
+    color:$BLACK;
+    padding:1.5rem;
+    background-color: hsl(191, 85%, 73%);
+    border: 2px solid;
+    border-color: white;
+}
+
+.theme {
+    font-size: 1.5rem;
+    font-weight: 600;
+    line-height: 1.125;
+}
+
+.line{
+    background-color:white;
+}
+
+.content{
+    width: 100%;
+    margin-bottom: 20px;
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,10 @@
   <section>
     <Hero />
     <BezierSvg1 />
+    <Jumbotron />
     <BezierSvg2 />
     <Main />
+    <Adcard />
   </section>
 </template>
 
@@ -13,13 +15,15 @@ import BezierSvg2 from '@/static/images/Curve2.svg'
 import Adcard from '@/components/Adcard'
 import Hero from '@/components/Hero'
 import Main from '@/components/Main'
+import Jumbotron from '@/components/Jumbotron'
 export default {
   components: {
     BezierSvg1,
     BezierSvg2,
     Adcard,
     Hero,
-    Main
+    Main,
+    Jumbotron
   }
 }
 </script>
