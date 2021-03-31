5b389571edf1dd4f53090afb671c822af65bc6cb
Mattun1212 <kouta.dvlp@icloud.com>
2021-03-31 22:19:10 +0900

    index.vueを調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,35 @@
 <template>
   <section>
     <Hero />
-    <BezierSvg1 />
     <Jumbotron />
-    <BezierSvg2 />
     <Main />
-    <Adcard />
   </section>
 </template>
 
 <script>
-import BezierSvg1 from '@/static/images/Curve.svg'
-import BezierSvg2 from '@/static/images/Curve2.svg'
-import Adcard from '@/components/Adcard'
 import Hero from '@/components/Hero'
 import Main from '@/components/Main'
 import Jumbotron from '@/components/Jumbotron'
 export default {
   components: {
-    BezierSvg1,
-    BezierSvg2,
-    Adcard,
     Hero,
     Main,
     Jumbotron
+  },
+  head() {
+
+     return {
+      title: 'トップ',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Here is description'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
