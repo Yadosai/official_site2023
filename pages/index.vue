47a78b02224399d518c51a66616267175753eb51
thistle519 <shizuru519@gmail.com>
2021-04-01 11:08:23 +0900

    index.vueについかしてある帯を一旦消した

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,13 @@
 <template>
   <section>
     <Header/>
-    <SubsubHeader/>
-    <SubHeader/>
     
   </section>
 </template>
 
 <script>
-import SubHeader from '@/components/SubHeader'
-import SubsubHeader from '@/components/SubHeader'
 export default {
   components: {
-    SubHeader
-
   }
 
 }
