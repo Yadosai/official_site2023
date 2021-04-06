15f16f8f4ad6947d9f2098d0dda8288b5be0b771
thistle519 <shizuru519@gmail.com>
2021-04-06 20:05:31 +0900

    余計なボタンを削除した

--- components/movie.vue
+++ components/movie.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <h1>
-      <a href="https://github.com/anteriovieira/vue-youtube">vue-youtube</a>
-    </h1>
     <youtube
       ref="youtube"
       :video-id="videoId"
@@ -10,9 +7,6 @@
       @ended="ended"
       @playing="playing"
     />
-    <button @click="playVideo">play</button>
-    <button @click="pauseVideo">pause</button>
-    <button @click="stopVideo">stop</button>
   </div>
 </template>
 
@@ -23,7 +17,7 @@ Vue.use(VueYoutube)
 export default {
   data() {
     return {
-      videoId: 'nteDXuqBfn0'
+      videoId: '_tNOc6wGqqg'
     }
   },
   computed: {
@@ -32,15 +26,6 @@ export default {
     }
   },
   methods: {
-    playVideo() {
-      this.player.playVideo()
-    },
-    pauseVideo() {
-      this.player.pauseVideo()
-    },
-    stopVideo() {
-      this.player.stopVideo()
-    },
     ready() {
       console.log('ready')
     },
