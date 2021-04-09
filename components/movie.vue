64e0397366db2c6ba2752a3959e1935e09bf3064
thistle519 <shizuru519@gmail.com>
2021-04-09 19:46:51 +0900

    iframeに方針を変更

--- components/movie.vue
+++ components/movie.vue
@@ -1,49 +1,28 @@
 <template>
   <div class="movie">
-    <youtube
-      ref="youtube"
-      class="flame"
-      :video-id="videoId"
-      @ready="ready"
-      @ended="ended"
-      @playing="playing"
-    />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/_tNOc6wGqqg"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
-
-<script>
-import Vue from 'vue'
-import VueYoutube from 'vue-youtube'
-Vue.use(VueYoutube)
-export default {
-  data() {
-    return {
-      videoId: '_tNOc6wGqqg'
-    }
-  },
-  computed: {
-    player() {
-      return this.$refs.youtube.player
-    }
-  },
-  methods: {
-    ready() {
-      console.log('ready')
-    },
-    ended() {
-      console.log('ended')
-    },
-    playing() {
-      console.log('playing')
-    }
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 .movie {
   text-align: center;
 }
-.flame {
+</style>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.movie {
+  text-align: center;
 }
 </style>
