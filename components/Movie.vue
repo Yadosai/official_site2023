be9f94187d6a417d00614502c968050cbab03cf1
thistle519 <shizuru519@gmail.com>
2021-04-09 19:56:53 +0900

    改めてiflameでmovieを追加

---
+++ components/Movie.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="movie">
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/_tNOc6wGqqg"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.movie {
+  text-align: center;
+}
+</style>
+
+<style lang="scss" scoped>
+.movie {
+  text-align: center;
+}
+</style>
