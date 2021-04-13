dc4eb3ecd1451089004e48bedb195a8022d7bb17
thistle519 <shizuru519@gmail.com>
2021-04-14 01:44:27 +0900

    新歓画像のコンポーネント作成

---
+++ components/ShinkanSchedule.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+      <div class="container fluid">
+        <div class="box">
+          <img src="images/info/shinkan.png"></img>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.fluid {
+  background-color: #89cfeb;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+
+.box {
+  text-align: center;
+}
+</style>
