543659dcf72ce96e0badc9b1f4909803bfd35e15
Hiroya Kobayashi <kobayashi.hiroya.qj@alumni.tsukuba.ac.jp>
2022-04-29 21:42:20 +0900

    ok

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,8 +22,7 @@
             <div class="contents">
               <p class="item">お祭り情報</p>
               <div class="line">
-                <p>
-                <nuxt-link to="/Yadosai">やどかり祭とは</nuxt-link>&nbsp;新歓について&nbsp;ニュース</p>
+                <p>やどかり祭とは&nbsp;新歓について&nbsp;ニュース</p>
               </div>
             </div>
           </div>
