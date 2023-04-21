4d689d824f929744483fc7bc1ca1b31b6fb9b3a6
Manato <namako.0149@gmail.com>
2023-04-22 00:14:57 +0900

    協賛団体変更

--- components/Adcard.vue
+++ components/Adcard.vue
@@ -1,18 +1,24 @@
 <template>
   <section>
     <div class="row justify-content-center">
-      <a href="https://mentoru.jp/info/tsukuba.html" target="_blank">
+      <!-- <a href="https://mentoru.jp/info/tsukuba.html" target="_blank">
         <img
           src="images/ad/めんとる.jpg"
           alt="sponsor"
           class="web-ad-content"
         />
-      </a>
-      <a href="http://www.seirock-ya.jp/" target="_blank">
+      </a> -->
+      <!-- <a href="http://www.seirock-ya.jp/" target="_blank">
         <img src="images/ad/清六家.jpg" alt="sponsor" class="web-ad-content" />
-      </a>
+      </a>  -->
       <a href="http://gakuen-menkyo.jp/" target="_blank">
       <img src="images/ad/学園中央自動車学校.png" alt="sponsor" class="web-ad-content" />
+      </a>
+       <a href="https://mentoru.jp/info/tsukuba.html" target="_blank">
+      <img src="images/ad/めんとるステーションつくば.jpg" alt="sponsor" class="web-ad-content" />
+      </a>
+      <a href="https://flow-kaikei.com/" target="_blank">
+      <img src="images/ad/FLOW会計事務所.png" alt="sponsor" class="web-ad-content" />
       </a>
       <a href="/Adlist">
         <img src="images/ad/etc.png" alt="sponsor" class="web-ad-content" />
