cf59b6c26963f7828d4128a685e0e7fd1c3470c5
Manato <namako.0149@gmail.com>
2023-05-24 20:17:37 +0900

    投票フォーム更新

--- components/SubsubHeader.vue
+++ components/SubsubHeader.vue
@@ -13,13 +13,13 @@ export default {}
 <style lang="scss" scoped>
 .pamphlet49 {
   font-size: 3vw;
-  background-color: #2e4b71;
+  background-color: #ffffff;
   padding: 1rem 0.5rem;
   margin: 1.5rem;
   text-align: center;
   border-radius: 10px;
 }
 a {
-  color: #ffffff;
+  color: #000000;
 }
 </style>
